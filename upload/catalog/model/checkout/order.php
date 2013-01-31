df3d700120156ba0387a5170d7c638cb5091ccbd
Ruslan Brest <rb@labtodo.com>
2013-01-31 08:35:15 +0200

    [!] fix email sending error if checkout form modified to make email field not required

--- upload/catalog/model/checkout/order.php
+++ upload/catalog/model/checkout/order.php
@@ -483,21 +483,24 @@ class ModelCheckoutOrder extends Model {
 
 			$text .= $language->get('text_new_footer') . "\n\n";
 		
-			$mail = new Mail(); 
-			$mail->protocol = $this->config->get('config_mail_protocol');
-			$mail->parameter = $this->config->get('config_mail_parameter');
-			$mail->hostname = $this->config->get('config_smtp_host');
-			$mail->username = $this->config->get('config_smtp_username');
-			$mail->password = $this->config->get('config_smtp_password');
-			$mail->port = $this->config->get('config_smtp_port');
-			$mail->timeout = $this->config->get('config_smtp_timeout');			
-			$mail->setTo($order_info['email']);
-			$mail->setFrom($this->config->get('config_email'));
-			$mail->setSender($order_info['store_name']);
-			$mail->setSubject(html_entity_decode($subject, ENT_QUOTES, 'UTF-8'));
-			$mail->setHtml($html);
-			$mail->setText(html_entity_decode($text, ENT_QUOTES, 'UTF-8'));
-			$mail->send();
+			if( !empty($order_info['email']) )
+			{
+				$mail = new Mail();
+				$mail->protocol = $this->config->get('config_mail_protocol');
+				$mail->parameter = $this->config->get('config_mail_parameter');
+				$mail->hostname = $this->config->get('config_smtp_host');
+				$mail->username = $this->config->get('config_smtp_username');
+				$mail->password = $this->config->get('config_smtp_password');
+				$mail->port = $this->config->get('config_smtp_port');
+				$mail->timeout = $this->config->get('config_smtp_timeout');
+				$mail->setTo($order_info['email']);
+				$mail->setFrom($this->config->get('config_email'));
+				$mail->setSender($order_info['store_name']);
+				$mail->setSubject(html_entity_decode($subject, ENT_QUOTES, 'UTF-8'));
+				$mail->setHtml($html);
+				$mail->setText(html_entity_decode($text, ENT_QUOTES, 'UTF-8'));
+				$mail->send();
+			}
 
 			// Admin Alert Mail
 			if ($this->config->get('config_alert_mail')) {
